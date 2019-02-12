66ce64772d8083a179109856a8ddaa9c2d46c3d7
OHTAKE Tomohiro <tomohiro.otake@deakin.edu.au>
2019-02-12 18:38:31 +1100

    Prepare VMZ

--- client/client.go
+++ client/client.go
@@ -11,9 +11,11 @@ func main() {
 	inputVideoPath := flag.String("input", "input", "Path of the input directory")
 	outputVideoPath := flag.String("output", "output", "Path of the output directory")
 	watchInterval := flag.Int("interval", 10, "Interval of watching input directory (sec)")
+	sshUser := flag.String("ssh-user", "", "user name for ssh connection to VMZ host")
+	sshHost := flag.String("ssh-host", "", "host name for ssh connection to VMZ host")
 	help := flag.Bool("help", false, "Print help")
 	flag.Parse()
-	if *help {
+	if *help || *sshUser == "" || *sshHost == "" {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -21,6 +23,13 @@ func main() {
 	fmt.Printf("%s (%d sec) -> %s\n", *inputVideoPath, *watchInterval, *outputVideoPath)
 
 	inputCh := make(chan string, 10)
+	videoAnalyzer := NewVideoAnalyzer(*sshUser, *sshHost, *inputVideoPath, *outputVideoPath, inputCh)
+	err := videoAnalyzer.PrepareVMZ()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "failure to prepare VMZ")
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(2)
+	}
 	go func() {
 		inputWatcher := NewInputWatcher(*inputVideoPath, inputCh)
 		for {
@@ -33,7 +42,6 @@ func main() {
 	}()
 
 	go func() {
-		videoAnalyzer := NewVideoAnalyzer(*inputVideoPath, *outputVideoPath, inputCh)
 		for {
 			err := videoAnalyzer.Next()
 			if err != nil {
--- client/video_analyzer.go
+++ client/video_analyzer.go
@@ -1,18 +1,71 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os/exec"
+)
 
 type VideoAnalyzer struct {
+	sshHost         string
+	sshUser         string
 	inputDirectory  string
 	outputDirectory string
 	inputCh         chan string
+	cmdVmz          *exec.Cmd
+	cmdVmzIn        io.WriteCloser
+	cmdVmzOut       io.ReadCloser
+	cmdVmzOutReader *bufio.Reader
+	cmdVmzErr       io.ReadCloser
 }
 
-func NewVideoAnalyzer(inputDirectory string, outputDirectory string, inputCh chan string) *VideoAnalyzer {
+func NewVideoAnalyzer(sshUser string, sshHost string, inputDirectory string, outputDirectory string, inputCh chan string) *VideoAnalyzer {
 	return &VideoAnalyzer{
+		sshUser:         sshUser,
+		sshHost:         sshHost,
 		inputDirectory:  inputDirectory,
 		outputDirectory: outputDirectory,
 		inputCh:         inputCh,
+	}
+}
+
+func (a *VideoAnalyzer) PrepareVMZ() error {
+	a.cmdVmz = exec.Command("ssh", fmt.Sprintf("%s@%s", a.sshUser, a.sshHost), ". /etc/profile && cd VMZ && . demo-0-initialize.sh && ./demo-1-analyze.sh ../tx2test.mp4")
+	var err error
+	a.cmdVmzIn, err = a.cmdVmz.StdinPipe()
+	if err != nil {
+		return err
+	}
+	a.cmdVmzOut, err = a.cmdVmz.StdoutPipe()
+	if err != nil {
+		return err
+	}
+	a.cmdVmzErr, err = a.cmdVmz.StderrPipe()
+	if err != nil {
+		return err
+	}
+	err = a.cmdVmz.Start()
+	if err != nil {
+		return err
+	}
+	a.cmdVmzOutReader = bufio.NewReader(a.cmdVmzOut)
+	for {
+		var line []byte
+		line, _, err = a.cmdVmzOutReader.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				stderrStr, _ := ioutil.ReadAll(a.cmdVmzErr)
+				return fmt.Errorf("EOF: stderr is: %s", stderrStr)
+			}
+			return err
+		}
+		lineStr := string(line)
+		if lineStr == "Press enter if you place a video file:" {
+			return nil
+		}
+		// log.Println(lineStr)
 	}
 }
 
